a8800f7b81f05b30d4cb2cb4ee689b9e43971b1b
Ryan Stradling <rstradling@boomtownroi.com>
2016-11-20 18:11:09 -0500

    Adding bulk strings to the parser

--- parser/src/main/scala/com/strad/free/redis/parser/RedisParser.scala
+++ parser/src/main/scala/com/strad/free/redis/parser/RedisParser.scala
@@ -6,19 +6,30 @@ sealed trait RedisResponses
 case class SimpleString(s: String) extends RedisResponses
 case class Error(s: String) extends RedisResponses
 case class LongResp(i: Long) extends RedisResponses
+trait BulkResponses extends RedisResponses
+case class BulkString(data: String) extends BulkResponses
+object BulkEmpty extends BulkResponses
+object BulkNull extends BulkResponses
 
 object RedisParser {
   def EOL = P("\r" ~ "\n")
   def intSentinel = P(":")
   def errorSentinel = P("-")
   def simpleSentinel = P("+")
+  def bulkSentinel = P("$")
   def textCanBeEmpty = P(CharsWhile(_ != '\r').rep.!)
   def textNotEmpty = P(CharsWhile(_ != '\r').!)
 
   def simpleString = simpleSentinel ~ textCanBeEmpty.map(s => SimpleString(s)) ~ EOL
   def error = errorSentinel ~ textCanBeEmpty.map(s => Error(s)) ~ EOL
   def integer = intSentinel ~ textNotEmpty.map(s => LongResp(s.toLong)) ~ EOL
+  def numBytes = P(CharIn('0' to '9').rep(1).!).map(_.toInt)
 
-  def redisResp = P(simpleString | integer | error)
+  def emptyBulk = P("$0\r\n\r\n").!.map(x => BulkEmpty)
+  def nullBulk = P("$-1\r\n").!.map(x => BulkNull)
+  def bulkNonEmptyHeader = P(bulkSentinel ~ numBytes ~ EOL).flatMap(count => AnyChars(count).!.map(y => BulkString(y)) ~ EOL)
+  def bulk = P(emptyBulk | nullBulk | bulkNonEmptyHeader).map((b: BulkResponses) => b)
+
+  def redisResp = P(simpleString | bulk | integer | error)
 
 }
--- parser/src/test/scala/com/strad/free/redis/parser/RedisParserSpec.scala
+++ parser/src/test/scala/com/strad/free/redis/parser/RedisParserSpec.scala
@@ -4,7 +4,23 @@ import fastparse.all._
 import org.scalatest._
 
 class RedisParserSpec extends FlatSpec with Matchers {
-  "RedisParser when given an Integer string" should "parse correctly" in {
+  "RedisParser when given a bulkString" should "parse correctly" in {
+    val str = "$3\r\nJOY\r\n"
+    val parser = RedisParser.bulk.parse(str)
+    parser shouldBe Parsed.Success(BulkString("JOY"), str.length)
+  }
+  it should "parse when given an empty bulkString" in {
+    val str = "$0\r\n\r\n"
+    val parser = RedisParser.bulk.parse(str)
+    parser shouldBe Parsed.Success(BulkEmpty, str.length)
+  }
+  it should "parse when given a null bulkString" in {
+    val str = "$-1\r\n"
+    val parser = RedisParser.bulk.parse(str)
+    parser shouldBe Parsed.Success(BulkNull, str.length)
+  }
+
+  "Redisparser when given an Integer string" should "parse correctly" in {
     val str = ":55\r\n"
     val parser = RedisParser.integer.parse(str)
     parser shouldBe Parsed.Success(LongResp(55), str.length)
@@ -44,4 +60,10 @@ class RedisParserSpec extends FlatSpec with Matchers {
     val parser = RedisParser.redisResp.parse(str)
     parser shouldBe Parsed.Success(LongResp(234), str.length)
   }
+  "RedisParser redisResp" should "be able to handle a BulkString" in {
+    val str = "$3\r\nJOY\r\n"
+    val parser = RedisParser.redisResp.parse(str)
+    parser shouldBe Parsed.Success(BulkString("JOY"), str.length)
+  }
+
 }
